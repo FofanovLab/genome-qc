416716e22a315861dce4d83f3c0a68febb0717ed
Andrew Sanchez <inbox.asanchez@gmail.com>
2019-05-13 14:04:00 -0700

    Split up output to avoid overloading ncbi

--- scripts/get_species_names.sh
+++ scripts/get_species_names.sh
@@ -2,11 +2,20 @@
 
 # Get values from species_taxid (column 7)
 # Skip the first two lines as they contain a comment and column names
+rm -f resources/*taxids*
 cut -f 7 resources/assembly_summary.txt| tail -n +3 |
-sort | uniq > resources/taxids.txt
+sort | uniq | split -d -l 10000 - resources/taxids_
 
-epost -db taxonomy -input resources/taxids.txt |
-efetch -format docsum |
-xtract -pattern DocumentSummary \
--group DocumentSummary -element ScientificName -element Id |
-sort > resources/names_and_taxids.txt
+#split -l 10000 resources/taxids.txt resources/taxids_
+
+for f in resources/taxids_*
+do
+    epost -db taxonomy -input $f |
+    efetch -format docsum |
+    xtract -pattern DocumentSummary \
+    -group DocumentSummary -element ScientificName -element Id |
+    sort >> $f.txt &&
+    sleep 2
+done
+
+cat resources/taxids*.txt >> resources/names_and_taxids.txt
